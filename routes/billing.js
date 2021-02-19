0450eec0b507820ecb5055e28f6a88b95d747c8b
abhishek0691 <abhishek.saxena1>
2021-02-19 22:44:46 +0530

    commit

--- routes/billing.js
+++ routes/billing.js
@@ -246,12 +246,12 @@ router.post('/api/bill/generateDealerBill', midWare.checkToken, (req, res, next)
 //     }).catch((err) => {
 //         console.log(err);
 //    });
-    const ht = pdfGeneration(req.decoded, req.body.selectedProducts, req.body.company, true);
-    console.log(ht);
-    pdf.create(ht).toStream((err, stream) => {
+    // const ht = pdfGeneration(req.decoded, req.body.selectedProducts, req.body.company, true);
+    // console.log(ht);
+    // pdf.create(ht).toStream((err, stream) => {
       
-                fs.createWriteStream('./public/html/'+fileName+'test'+'.pdf');
-              })
+    //             fs.createWriteStream('./public/html/'+fileName+'test'+'.pdf');
+    //           })
     
         
            
