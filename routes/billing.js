1938d91dfdb70b5c53e990deb210e208e86469c1
abhishek0691 <abhishek.saxena1>
2021-02-19 22:41:37 +0530

    commit

--- routes/billing.js
+++ routes/billing.js
@@ -182,6 +182,23 @@ router.post('/api/bill/generateDealerBill', midWare.checkToken, (req, res, next)
     }
     req.decoded['totalPrice'] = totalPrice;
     const fileName = "FootWear_"+ new Date().getTime()+'_'+req.decoded.mobile;
+    var options = {
+        format: "A4",
+        orientation: "portrait",
+        border: "10mm",
+        header: {
+            height: "45mm",
+            contents: '<div style="text-align: center;">Author: Shyam Hajare</div>'
+        },
+        "footer": {
+            "height": "28mm",
+            "contents": {
+            first: 'Cover page',
+            2: 'Second page', // Any page number is working. 1-based index
+            default: '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
+            last: 'Last Page'
+        }
+    };
     var document = {
         html: html,
         data: { },
