1ce949cc5a910b626886d66f348e2d62cc826308
abhishek0691 <abhishek.saxena1>
2021-02-24 20:42:37 +0530

    commit

--- public/delivery-delivery-module-es5.js
+++ public/delivery-delivery-module-es5.js
@@ -453,8 +453,8 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
 
       //srvLink: string = "http://localhost:8080/";
       //srvLink: string = "http://18.220.168.114:8080/"
-      this.srvLink = "http://34.219.112.19:8080/"; //srvLink: string = 'https://merchant.softdocket.com/';
-      //srvLink: string = "http://retail.mobinyx.com/";
+      //srvLink: string = "https://34.219.112.19:8080/"
+      this.srvLink = 'https://merchant.softdocket.com/'; //srvLink: string = "http://retail.mobinyx.com/";
       // srvLink: string = "http://35.154.98.22:8080/";
       //srvLink: string = "https://dealer.mobinyx.com/"
 
